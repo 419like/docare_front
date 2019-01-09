9916ff40046b00dc6248aa9d1fb62f36f553301c
chenyang <137485352@qq.com>
2019-01-09 14:58:25 +0800

    1458

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 7080,
+    port: 8080,
 	host:'0.0.0.0',
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
--- src/components/operationMaster/operationMaster.vue
+++ src/components/operationMaster/operationMaster.vue
@@ -186,7 +186,7 @@
           </div>
           <div v-if="concealmentFourData" style="padding:5px;display:flex;flex-wrap:wrap;">
             <button class="list_button">系统配置</button>
-            <button @click="getAboutUs" class="list_button">关于</button>
+            <!-- <button @click="getAboutUs" class="list_button">关于</button> -->
             <button @click="exitSystem" class="list_button">返回列表</button>
             <button @click="openChangePassWordView" class="list_button">修改密码</button>
           </div>

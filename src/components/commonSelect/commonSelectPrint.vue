f13b66a39d33afecefc5edd2dfa8be37710ca536
BigQuestion <176562986@qq.com>
2019-04-17 15:02:46 +0800

    1

--- src/components/commonSelect/commonSelectPrint.vue
+++ src/components/commonSelect/commonSelectPrint.vue
@@ -1,98 +1,42 @@
 <template>
   <div style="position: relative;font-family: SimSun;font-weight:bold;">
-    <div
-      v-if="conInfo.bottomLineMode&&conInfo.lineTypeMode=='solid'&&conInfo.strFormatMode==''"
-      :style="{width:conInfo.width+'px',color:conInfo.ForeColor,cursor:conInfo.cursorMode,opacity:conInfo.opacity,border:'0',textAlign:conInfo.TextAlignMode,}"
-      style="min-width: 20px;min-height: 20px;position:relative;"
-    >
+    <div v-if="conInfo.bottomLineMode&&conInfo.lineTypeMode=='solid'&&conInfo.strFormatMode==''" :style="{width:conInfo.width+'px',color:conInfo.ForeColor,cursor:conInfo.cursorMode,opacity:conInfo.opacity,border:'0',textAlign:conInfo.TextAlignMode,}" style="min-width: 20px;min-height: 20px;position:relative;">
       <span>{{conInfo[attrName]}}</span>
       <div style="position:absolute;bottom:-22px;">
-        <svg
-          height="20"
-          :style="{width:conInfo.width+'px'}"
-        >
-          <g
-            fill="none"
-            stroke="black"
-            stroke-width="1"
-          >
-            <path
-              stroke-dasharray="5,5"
-              d="M0 0 l900 0"
-            />
+        <svg height="20" :style="{width:conInfo.width+'px'}">
+          <g fill="none" stroke="black" stroke-width="1">
+            <path stroke-dasharray="5,5" d="M0 0 l900 0" />
           </g>
         </svg>
       </div>
     </div>
-    <div
-      v-else-if="conInfo.bottomLineMode&&conInfo.lineTypeMode=='dashed'&&conInfo.strFormatMode==''"
-      :style="{width:conInfo.width+'px',cursor:conInfo.cursorMode,opacity:conInfo.opacity,textAlign:conInfo.TextAlignMode,fontSize:conInfo.fontSize+'pt'}"
-      style="min-width: 20px;min-height: 20px;position:relative;"
-    >
+    <div v-else-if="conInfo.bottomLineMode&&conInfo.lineTypeMode=='dashed'&&conInfo.strFormatMode==''" :style="{width:conInfo.width+'px',cursor:conInfo.cursorMode,opacity:conInfo.opacity,textAlign:conInfo.TextAlignMode,fontSize:conInfo.fontSize+'pt'}" style="min-width: 20px;min-height: 20px;position:relative;">
       {{conInfo[attrName]}}
       <div style="position:absolute;bottom:-22px;">
-        <svg
-          height="20"
-          :style="{width:conInfo.width+'px'}"
-        >
-          <g
-            fill="none"
-            stroke="black"
-            stroke-width="1"
-          >
-            <path
-              stroke-dasharray="5,5"
-              d="M0 0 l900 0"
-            />
+        <svg height="20" :style="{width:conInfo.width+'px'}">
+          <g fill="none" stroke="black" stroke-width="1">
+            <path stroke-dasharray="5,5" d="M0 0 l900 0" />
           </g>
         </svg>
       </div>
     </div>
     <!-- <input v-model="strToDate"   :readonly="true"> -->
-    <div
-      v-else-if="conInfo.strFormatMode == 'yyyy-MM-dd'||conInfo.strFormatMode == 'hh:mm'||conInfo.strFormatMode == 'yyyy-MM-dd hh:mm'"
-      :style="{width:conInfo.width+'px',cursor:conInfo.cursorMode,opacity:conInfo.opacity,fontSize:conInfo.fontSize+'pt',textAlign:conInfo.TextAlignMode}"
-      style="min-width: 20px;min-height: 20px;position:relative;"
-    >
+    <div v-else-if="conInfo.strFormatMode == 'yyyy-MM-dd'||conInfo.strFormatMode == 'hh:mm'||conInfo.strFormatMode == 'yyyy-MM-dd hh:mm'" :style="{width:conInfo.width+'px',cursor:conInfo.cursorMode,opacity:conInfo.opacity,fontSize:conInfo.fontSize+'pt',textAlign:conInfo.TextAlignMode}" style="min-width: 20px;min-height: 20px;position:relative;">
       <span>{{strToDate}}</span>
       <div style="position:absolute;bottom:-22px;">
-        <svg
-          height="20"
-          :style="{width:conInfo.width+'px'}"
-        >
-          <g
-            fill="none"
-            stroke="black"
-            stroke-width="1"
-          >
-            <path
-              stroke-dasharray="5,5"
-              d="M0 0 l900 0"
-            />
+        <svg height="20" :style="{width:conInfo.width+'px'}">
+          <g fill="none" stroke="black" stroke-width="1">
+            <path stroke-dasharray="5,5" d="M0 0 l900 0" />
           </g>
         </svg>
       </div>
     </div>
-    <div
-      v-else
-      :style="{width:conInfo.width+'px',cursor:conInfo.cursorMode,opacity:conInfo.opacity,textAlign:conInfo.TextAlignMode,fontSize:conInfo.fontSize}"
-      style="min-width: 20px;min-height: 20px;"
-    >
+    <div v-else :style="{width:conInfo.width+'px',cursor:conInfo.cursorMode,opacity:conInfo.opacity,textAlign:conInfo.TextAlignMode,fontSize:conInfo.fontSize}" style="min-width: 20px;min-height: 20px;">
       <span>{{conInfo[attrName]}}</span>
       <div style="position:absolute;bottom:-22px;">
-        <svg
-          height="30"
-          :style="{width:conInfo.width+'px'}"
-        >
-          <g
-            fill="none"
-            stroke="black"
-            stroke-width="1"
-          >
-            <path
-              stroke-dasharray="5,5"
-              d="M0 0 l900 0"
-            />
+        <svg height="30" :style="{width:conInfo.width+'px'}">
+          <g fill="none" stroke="black" stroke-width="1">
+            <path stroke-dasharray="5,5" d="M0 0 l900 0" />
           </g>
         </svg>
       </div>
@@ -153,7 +97,7 @@ export default {
     }
   },
   props: ["conInfo", "attrName", "data"],
-  mounted() {},
+  mounted() { },
   created() {
     // 点击其他不在的区域触发事件
     document.addEventListener("click", e => {
